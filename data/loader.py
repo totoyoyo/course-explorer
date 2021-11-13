8c8d2d5bb49053819210b5e407397fb4eeec8261
noa <hi@noa.dev>
2021-11-13 10:23:41 -0800

    result fetching with clock

--- data/loader.py
+++ data/loader.py
@@ -1,9 +1,10 @@
 import db_connection
 import model.Student as student
+import model.Clock as clock
 
 students = []
 
 for row in db_connection.sql('SELECT * from users_scores'):
-    s = student.make_student(row, db_connection)
+    s = student.make_student(row, db_connection, clock.Clock(9999999999999999999))
     students.append(s)
-    print(s.contributions())
+    print(s.autotest_results())
--- data/model/Student.py
+++ data/model/Student.py
@@ -1,21 +1,25 @@
 class Student:
 
-    def __init__(self, conn, anon_id, lab_time, withdrawn, score):
+    def __init__(self, conn, clock, anon_id, lab_time, withdrawn, score):
         self.conn = conn
         self.anon_id = anon_id
         self.lab_time = lab_time
         self.withdrawn = withdrawn
         self.score = score
+        self.clock = clock
 
     def visits(self):
-        return self.conn.sql('SELECT * from queue_visits WHERE anon_id = \"' + self.anon_id + '\"')
+        return self.conn.sql('SELECT * from queue_visits WHERE anon_id = \"' + self.anon_id + '\" AND dequeue <' + str(self.clock.time))
 
     def contributions(self):
-        return self.conn.sql('SELECT * from contributions WHERE anon_id = \"' + self.anon_id + '\"')
+        return self.conn.sql('SELECT * from contributions WHERE anon_id = \"' + self.anon_id + '\" AND created_at <' + str(self.clock.time))
+
+    def autotest_results(self):
+        return self.conn.sql('SELECT res.* FROM autotest_results res, result_membership mem WHERE mem.anon_id = \"' + self.anon_id + '\" AND mem.feedback_id = res.feedback_id AND res.request_time <' + str(self.clock.time))
 
 
 
 
-def make_student(row, conn):
-    student = Student(conn, row[0], row[1], row[2], row[3])
+def make_student(row, conn, clock):
+    student = Student(conn, clock, row[0], row[1], row[2], row[3])
     return student
