1d692ea2771e8ecd4024acf2308e8926dfcd3796
noa <hi@noa.dev>
2021-12-07 08:34:07 -0800

    new things

---
+++ backend/data/loader.py
@@ -0,0 +1,10 @@
+import db_connection
+import model.defs as student
+import model.Clock as clock
+
+students = []
+
+for row in db_connection.sql('SELECT * from users_scores'):
+    s = student.make_student(row, db_connection, clock.Clock(9999999999999999999))
+    students.append(s)
+    print(s.autotest_results())
