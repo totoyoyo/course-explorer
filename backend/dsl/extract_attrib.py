4f561728c7b102906eb161aae3b7fad13e317ba0
noa <hi@noa.dev>
2021-12-09 21:54:22 -0800

    added new visitor for extracting attributes

---
+++ backend/dsl/extract_attrib.py
@@ -0,0 +1,92 @@
+from antlr.DSLGrammarLexer import DSLGrammarLexer
+from antlr.DSLGrammarParser import DSLGrammarParser
+from antlr.DSLGrammarVisitor import DSLGrammarVisitor
+from antlr4.error.ErrorListener import ErrorListener
+from antlr4 import InputStream, CommonTokenStream
+from datetime import datetime
+import data.model.attribute.helpers as attr
+import data.model.attribute.factory as fact
+
+
+class AttributeExtractingVisitor(DSLGrammarVisitor):
+
+    def __init__(self):
+        super().__init__()
+
+    def visitQuery(self, ctx: DSLGrammarParser.QueryContext):
+        # fill this
+        return super().visitQuery(ctx)
+
+    def visitSome_filter(self, ctx: DSLGrammarParser.Some_filterContext):
+        # Some sample outline
+        return super().visitSome_filter(ctx)
+
+    def visitLogic(self, ctx: DSLGrammarParser.LogicContext):
+        return super().visitLogic(ctx)
+
+    def visitBinary(self, ctx: DSLGrammarParser.BinaryContext):
+        return super().visitBinary(ctx)
+
+    def visitComparable(self, ctx: DSLGrammarParser.ComparableContext):
+        return super().visitComparable(ctx)
+
+    def visitNumber(self, ctx: DSLGrammarParser.NumberContext):
+        return super().visitNumber(ctx)
+
+    def visitGranularity_result(self,
+                                ctx: DSLGrammarParser.Granularity_resultContext):
+        return super().visitGranularity_result(ctx)
+
+
+    def visitAggr_op(self, ctx:DSLGrammarParser.Aggr_opContext):
+        return self.visitAggr_op(ctx)
+
+    def visitArithmetic(self, ctx: DSLGrammarParser.ArithmeticContext):
+        return self.visitArithmetic(ctx)
+
+    def visitString(self, ctx: DSLGrammarParser.StringContext):
+        return super().visitString(ctx)
+
+    def visitTime(self, ctx: DSLGrammarParser.TimeContext):
+        return super().visitTime(ctx)
+
+    def visitStudent_attribute(self,
+                               ctx: DSLGrammarParser.Student_attributeContext):
+        return super().visitStudent_attribute(ctx)
+
+    def visitAttribute(self, ctx: DSLGrammarParser.AttributeContext):
+        return ctx.getText()
+
+    def visitTime_lit(self, ctx: DSLGrammarParser.Time_litContext):
+        return super().visitTime_lit(ctx)
+
+    # we build a set of all the attributes
+    def aggregateResult(self, aggregate, nextResult):
+        if not isinstance(aggregate, set):
+            aggregate = set()
+        if nextResult is not None:
+            aggregate.add(nextResult)
+        return aggregate
+
+
+class OurErrorListener(ErrorListener):
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise Exception("Something happened")
+
+
+def run_query(query):
+    data = InputStream(query)
+    # for throwing exception
+    error_listener = OurErrorListener()
+
+    # Lexer
+    lexer = DSLGrammarLexer(data)
+    lexer.addErrorListener(error_listener)
+    stream = CommonTokenStream(lexer)
+
+    # Parser
+    parser = DSLGrammarParser(stream)
+    parser.addErrorListener(error_listener)
+    tree = parser.query()
+
+    return AttributeExtractingVisitor().visit(tree)
